1e418d82d251025852d15c7640d4ed4884a4a74e
AnushkaYadav29 <anushkayadav29032003@gmail.com>
2026-03-08 16:28:02 +0530

    Task 1 - Operators and Expressions completed

---
+++ Assignments/Task1_Operators_Expression/Ass4.js
@@ -0,0 +1,19 @@
+
+let x=10;
+console.log("The initial value of x : "+x);
+
+x+=5;
+console.log("After Adding : "+x);
+
+x-=10;
+console.log("After Subtracting : "+x);
+
+x*=3;
+console.log("After Multiply : "+x);
+
+x/=2;
+console.log("After Division : "+x);
+
+x%=3;
+console.log("After Modulus : "+x);
+
---
+++ Assignments/Task1_Operators_Expression/Ass5.js
@@ -0,0 +1,4 @@
+console.log(10+3*5);
+console.log((10+3)*5);
+console.log(20/2+3);
+console.log(20/(2+3));
