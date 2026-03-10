ffa55e16bec621332cf178bfd33485faceead6a0
AnushkaYadav29 <anushkayadav29032003@gmail.com>
2026-03-10 23:52:25 +0530

    Conditions and loops assignment completed

---
+++ Assignments/Practice_JS/pattern.js
@@ -0,0 +1,59 @@
+
+for(i=1;i<=5;i++){
+    for(j=1;j<=5;j++){
+        ("*")
+    }
+    console.log
+}
+
+
+// function printSquare(n){
+//     for(i=1;i<=n;i++){
+//          line =' ';
+//         for(j=1;j<=n;j++){
+//             line+="* "
+//         }
+//         console.log(line)
+//     }
+// }
+
+// printSquare(5)
+
+
+// function printTriangle(n){
+//     for(i=1;i<=n;i++){
+//          line =' ';
+//         for(j=1;j<=i;j++){
+//             line+="* "
+//         }
+//         console.log(line)
+//     }
+// }
+
+// printTriangle(5)
+
+
+// function printRevTriangle(n){
+//     for(i=1;i<=n;i++){
+//          line =' ';
+//         for(j=1;j<=n-i+1;j++){
+//             line+="* "
+//         }
+//         console.log(line)
+//     }
+// }
+
+// printRevTriangle(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
